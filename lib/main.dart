f4e4aa08d43e24f381a6f0b19ad482ee55aa88b3
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-05-26 23:06:20 +0600

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class _WeatherAppState extends State<WeatherApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Column(
           children: <Widget>[
@@ -19,7 +20,7 @@ class _WeatherAppState extends State<WeatherApp> {
             ),
             // SizedBox(height: 50.0),
             Container(
-              // margin: EdgeInsets.only(top: 50.0),
+              margin: EdgeInsets.only(top: 50.0),
               child: Text(
                 'You are in: ',
                 style: TextStyle(
@@ -27,6 +28,42 @@ class _WeatherAppState extends State<WeatherApp> {
                   fontWeight: FontWeight.bold,
                   color: Colors.blue[500],
                 ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      'Bangladesh',
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[500],
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10.0),
+                  Container(
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                      size: 35.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(Icons.wb_sunny, color: Colors.amber),
+                title: Text('Temp 25 C'),
               ),
             ),
           ],
