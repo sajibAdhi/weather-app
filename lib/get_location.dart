ec3624e05025836f3e5528818abdddb15aef481f
sajibAdhi <sajibadhikery@gmail.com>
2021-07-26 16:50:50 +0600

    Update GetLocation Class

--- lib/get_location.dart
+++ lib/get_location.dart
@@ -1,36 +1,14 @@
-import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 
 class GetLocation {
-  double latitude;
-  double longitude;
-  String city;
   // Get Current Location
-  Future<void> getCurrentLocation() async {
+  Future getCurrentLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      latitude = position.latitude;
-      longitude = position.longitude;
-      city = await getCityName(position.latitude, position.longitude);
+      Position position = await Geolocator.getCurrentPosition( desiredAccuracy: LocationAccuracy.high );
+
+      return position;
     } catch (e) {
       print(e);
     }
   }
-
-  // Get City Name
-  Future<String> getCityName(double lat, double lon) async {
-    List<Placemark> placemarks = [];
-
-    try {
-      placemarks = await placemarkFromCoordinates(lat, lon, localeIdentifier: "en");
-      print('City Name is: ${placemarks[0].locality}');
-    } catch (error) {
-      print("getCityName error: $error");
-    }
-    return placemarks[0].locality;
-
-  }
-
- 
 }
