c886f0fc4a507d7b91db22fe24204d245fc2d864
sajibAdhi <sajibadhikery@gmail.com>
2021-07-28 18:14:41 +0600

    bugfixed

--- lib/http_handler.dart
+++ lib/http_handler.dart
@@ -7,8 +7,8 @@ class HttpHandler {
   static const String _apiKey = '0769fb37917d0ed73656a2e17095965b';
   static GetLocation _getLocation = GetLocation();
 
-  // Get Temp
-  static Future<WeatherData> getTemp() async {
+  // Get http Data
+  static Future<WeatherData> getHttpData() async {
     var _getLatLoc = await _getLocation.getCurrentLocation();
 
     if (_getLatLoc != null) {
@@ -16,8 +16,8 @@ class HttpHandler {
           'http://api.openweathermap.org/data/2.5/weather?lat=${_getLatLoc.latitude}&lon=${_getLatLoc.longitude}&appid=$_apiKey&units=metric';
       var url = Uri.parse(uri);
       http.Response response = await http.get(url);
+
       if (response.statusCode == 200) {
-        print("get Temp ${WeatherData.fromJSON(json.decode(response.body))}");
         return WeatherData.fromJSON(json.decode(response.body));
       }
     }
--- lib/weather_data.dart
+++ lib/weather_data.dart
@@ -1,17 +1,17 @@
 class WeatherData {
   final String city;
   final String description;
-  final int temp;
+  final double temp;
   final String icon;
 
   WeatherData(
       {required this.city,
       required this.description,
-      required this.temp,
-      required this.icon});
+      required this.icon,
+      required this.temp});
 
-  factory WeatherData.fromJSON(Map json) {
-    return WeatherData(
+  factory WeatherData.fromJSON(Map<String, dynamic> json) {
+    return new WeatherData(
         city: json['name'],
         description: json['weather'][0]['description'],
         icon: json['weather'][0]['icon'],
